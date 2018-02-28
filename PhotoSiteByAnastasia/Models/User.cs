57faedc96d174bda2a999284a504144863b6ac8d
alender07 <alender07@yandex.ru>
2018-02-28 19:19:39 +0400

    ...

---
+++ PhotoSiteByAnastasia/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSiteByAnastasia.Models
+{
+    public class User
+    {
+        public int UserId;
+        public string Login;
+        public string Password;
+        public string Name;
+        public string Email;
+        public string City;
+    }
+}
