600f6d0585b42fe2711e44d89a90078432a71f68
alender07 <alender07@yandex.ru>
2018-03-13 02:42:15 +0400

    Возможность отправки почты

---
+++ PhotoSiteByAnastasia/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotoSiteByAnastasia.Models;
+using ActionMailer.Net.Mvc;
+
+namespace PhotoSiteByAnastasia.Controllers
+{
+    public class EmailController : MailerBase
+    {
+             
+
+        public EmailResult SendEmail(EmailModel model)
+        {
+            To.Add(model.To);
+
+            From = model.From;
+
+            Subject = model.Subject;
+
+            return Email("SendEmail", model);
+        }
+
+        
+    }
+}
--- PhotoSiteByAnastasia/Controllers/HomeController.cs
+++ PhotoSiteByAnastasia/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PhotoSiteByAnastasia.Models;
+using ActionMailer.Net.Mvc;
 
 namespace PhotoSiteByAnastasia.Controllers
 {
@@ -34,6 +36,35 @@ namespace PhotoSiteByAnastasia.Controllers
         }
 
         public ActionResult Entry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Entry(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    new EmailController().SendEmail(model).Deliver();
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+                    return RedirectToAction("Error");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
         {
             return View();
         }
